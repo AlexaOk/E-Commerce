b4906f40c829aed5ed3db4d0234895ad44ff5243
Louise <louise.roussel@epitech.eu>
2018-06-18 16:10:58 +0200

    CategoriesController

--- e-commerce/src/Controller/CategoriesController.php
+++ e-commerce/src/Controller/CategoriesController.php
@@ -3,8 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Categories;
+use App\Entity\Products;
 use App\Form\CategoriesType;
 use App\Repository\CategoriesRepository;
+use App\Repository\ProductsRepository;
+use App\Repository\SubCategoriesRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -23,70 +27,78 @@ class CategoriesController extends Controller
         return $this->render('categories/index.html.twig', ['categories' => $categoriesRepository->findAll()]);
     }
 
-    /**
-     * @Route("/new", name="categories_new", methods="GET|POST")
-     */
-    public function new(Request $request): Response
-    {
-        $category = new Categories();
-        $form = $this->createForm(CategoriesType::class, $category);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($category);
-            $em->flush();
-
-            return $this->redirectToRoute('categories_index');
-        }
-
-        return $this->render('categories/new.html.twig', [
-            'category' => $category,
-            'form' => $form->createView(),
-        ]);
-    }
+//    /**
+//     * @Route("/new", name="categories_new", methods="GET|POST")
+//     */
+//    public function new(Request $request): Response
+//    {
+//        $category = new Categories();
+//        $form = $this->createForm(CategoriesType::class, $category);
+//        $form->handleRequest($request);
+//
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $em = $this->getDoctrine()->getManager();
+//            $em->persist($category);
+//            $em->flush();
+//
+//            return $this->redirectToRoute('categories_index');
+//        }
+//
+//        return $this->render('categories/new.html.twig', [
+//            'category' => $category,
+//            'form' => $form->createView(),
+//        ]);
+//    }
 
     /**
      * @Route("/{slug}", name="categories_show", methods="GET")
+     * @ParamConverter("slug", options={"mapping": {"category_name" : "name"}})
      */
-    public function show($slug): Response
+    public function show($slug, CategoriesRepository $categoriesRepository, SubCategoriesRepository $subCategoriesRepository, ProductsRepository $productsRepository): Response
     {
-        $category = $categoriesRepository->findBy()
-        var_dump($category);
-        return $this->render('categories/show.html.twig', ['category' => $category]);
-    }
+        $category = $categoriesRepository->findBy(array('name' => $slug));
+        $products = $productsRepository->find($category);
+        // dump($subCategoriesRepository->findBy(array('category' => $category[0])));
+        dump($products);
+        return $this->render('categories/show.html.twig', [
+            'category' => $category[0],
+            'categories' => $categoriesRepository->findAll(),
+            'sub_categories' => $subCategoriesRepository->findBy(array('category' => $category[0])),
 
-    /**
-     * @Route("/{id}/edit", name="categories_edit", methods="GET|POST")
-     */
-    public function edit(Request $request, Categories $category): Response
-    {
-        $form = $this->createForm(CategoriesType::class, $category);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('categories_edit', ['id' => $category->getId()]);
-        }
-
-        return $this->render('categories/edit.html.twig', [
-            'category' => $category,
-            'form' => $form->createView(),
         ]);
     }
 
-    /**
-     * @Route("/{id}", name="categories_delete", methods="DELETE")
-     */
-    public function delete(Request $request, Categories $category): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$category->getId(), $request->request->get('_token'))) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($category);
-            $em->flush();
-        }
-
-        return $this->redirectToRoute('categories_index');
-    }
+//    /**
+//     * @Route("/{id}/edit", name="categories_edit", methods="GET|POST")
+//     */
+//    public function edit(Request $request, Categories $category): Response
+//    {
+//        $form = $this->createForm(CategoriesType::class, $category);
+//        $form->handleRequest($request);
+//
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $this->getDoctrine()->getManager()->flush();
+//
+//            return $this->redirectToRoute('categories_edit', ['id' => $category->getId()]);
+//        }
+//
+//        return $this->render('categories/edit.html.twig', [
+//            'category' => $category,
+//            'form' => $form->createView(),
+//        ]);
+//    }
+//
+//    /**
+//     * @Route("/{id}", name="categories_delete", methods="DELETE")
+//     */
+//    public function delete(Request $request, Categories $category): Response
+//    {
+//        if ($this->isCsrfTokenValid('delete'.$category->getId(), $request->request->get('_token'))) {
+//            $em = $this->getDoctrine()->getManager();
+//            $em->remove($category);
+//            $em->flush();
+//        }
+//
+//        return $this->redirectToRoute('categories_index');
+//    }
 }
--- e-commerce/src/Controller/ProductsController.php
+++ e-commerce/src/Controller/ProductsController.php
@@ -29,28 +29,28 @@ class ProductsController extends Controller
         ]);
     }
 
-    /**
-     * @Route("/new", name="products_new", methods="GET|POST")
-     */
-    public function new(Request $request): Response
-    {
-        $product = new Products();
-        $form = $this->createForm(ProductsType::class, $product);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($product);
-            $em->flush();
-
-            return $this->redirectToRoute('products_index');
-        }
-
-        return $this->render('products/new.html.twig', [
-            'product' => $product,
-            'form' => $form->createView(),
-        ]);
-    }
+//    /**
+//     * @Route("/new", name="products_new", methods="GET|POST")
+//     */
+//    public function new(Request $request): Response
+//    {
+//        $product = new Products();
+//        $form = $this->createForm(ProductsType::class, $product);
+//        $form->handleRequest($request);
+//
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $em = $this->getDoctrine()->getManager();
+//            $em->persist($product);
+//            $em->flush();
+//
+//            return $this->redirectToRoute('products_index');
+//        }
+//
+//        return $this->render('products/new.html.twig', [
+//            'product' => $product,
+//            'form' => $form->createView(),
+//        ]);
+//    }
 
     /**
      * @Route("/{id}", name="products_show", methods="GET")
@@ -60,37 +60,37 @@ class ProductsController extends Controller
         return $this->render('products/show.html.twig', ['product' => $product]);
     }
 
-    /**
-     * @Route("/{id}/edit", name="products_edit", methods="GET|POST")
-     */
-    public function edit(Request $request, Products $product): Response
-    {
-        $form = $this->createForm(ProductsType::class, $product);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('products_edit', ['id' => $product->getId()]);
-        }
-
-        return $this->render('products/edit.html.twig', [
-            'product' => $product,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="products_delete", methods="DELETE")
-     */
-    public function delete(Request $request, Products $product): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$product->getId(), $request->request->get('_token'))) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($product);
-            $em->flush();
-        }
-
-        return $this->redirectToRoute('products_index');
-    }
+//    /**
+//     * @Route("/{id}/edit", name="products_edit", methods="GET|POST")
+//     */
+//    public function edit(Request $request, Products $product): Response
+//    {
+//        $form = $this->createForm(ProductsType::class, $product);
+//        $form->handleRequest($request);
+//
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $this->getDoctrine()->getManager()->flush();
+//
+//            return $this->redirectToRoute('products_edit', ['id' => $product->getId()]);
+//        }
+//
+//        return $this->render('products/edit.html.twig', [
+//            'product' => $product,
+//            'form' => $form->createView(),
+//        ]);
+//    }
+//
+//    /**
+//     * @Route("/{id}", name="products_delete", methods="DELETE")
+//     */
+//    public function delete(Request $request, Products $product): Response
+//    {
+//        if ($this->isCsrfTokenValid('delete'.$product->getId(), $request->request->get('_token'))) {
+//            $em = $this->getDoctrine()->getManager();
+//            $em->remove($product);
+//            $em->flush();
+//        }
+//
+//        return $this->redirectToRoute('products_index');
+//    }
 }
